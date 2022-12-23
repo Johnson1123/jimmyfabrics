7afd2c7a3feb993ab72885cd9fd8356a2a0fbc6e
unknown <onifadejohnson2017@gmail.com>
2022-12-23 12:27:40 -0500

    uploading folder

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h1>Hello Ruth</h1>
+    <h1>Hello World</h1>
   </React.StrictMode>
 );
 
