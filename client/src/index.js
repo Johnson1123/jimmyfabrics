0ee2caf7985afd5bade925dede1f0a42ca55c361
unknown <onifadejohnson2017@gmail.com>
2022-12-23 15:25:06 -0500

    add pages and style

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './style/app.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h1>Hello World</h1>
+    <h1 className='p__text'>Hello World</h1>
   </React.StrictMode>
 );
 
